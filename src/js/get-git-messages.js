d91b8d47b1bbefeea953713f2b0770ae8cf3d7ae
Andrew Schorr <afs35mm@gmail.com>
2019-02-20 10:49:51 -0500

    Added welcome message 🎉 😃 🚒

--- src/js/get-git-messages.js
+++ src/js/get-git-messages.js
@@ -5,21 +5,22 @@ const endpoint = 'https://api.github.com/users/andrewfschorr/events';
 
 function getRecentGitCommits(cb) {
     fetch(endpoint).then(resp => resp.json()).then((data) => {
-        console.log(data);
-        const pushEvents = data.filter((evt) => {
-            return evt.type === 'PushEvent';
-        });
-        const commitObjs = [];
-        pushEvents.forEach((pushEvt) => {
-            pushEvt.payload.commits.forEach((commit) => {
-                commitObjs.push({
-                    commit: commit.message,
-                    dateTime: moment(pushEvt.created_at).format('dddd, MMMM Do YYYY, h:mm:ss a'),
-                    repo: pushEvt.repo.name,
-                    commitUrl: `https://github.com/${pushEvt.repo.name}/commit/${commit.sha}`,
-                })
-            })
-        });
+      console.log('Welcomet to my website! It looks like someone knows how to use the console! 🎉 😃 🚒')
+      console.log(data);
+      const pushEvents = data.filter((evt) => {
+          return evt.type === 'PushEvent';
+      });
+      const commitObjs = [];
+      pushEvents.forEach((pushEvt) => {
+          pushEvt.payload.commits.forEach((commit) => {
+              commitObjs.push({
+                  commit: commit.message,
+                  dateTime: moment(pushEvt.created_at).format('dddd, MMMM Do YYYY, h:mm:ss a'),
+                  repo: pushEvt.repo.name,
+                  commitUrl: `https://github.com/${pushEvt.repo.name}/commit/${commit.sha}`,
+              })
+          })
+      });
     cb(commitObjs);
     }).catch((err) => {
         cb(data);
